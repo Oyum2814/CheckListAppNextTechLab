4954c2dc1aec3cc46afab2906157aa0e45d1e1f4
Om <ommukherjee28@gmail.com>
2021-10-17 17:23:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ for (i = 0; i < close.length; i++) {
 // Add a "checked" symbol when clicking on a list item
 var list = document.querySelector('ul');
 list.addEventListener('click', function(ev) {
-  if (ev.target.tagName === 'LI') {
+  if (ev.target.tagName === 'H3') {
     ev.target.classList.toggle('checked');
   }
 },false);
@@ -38,18 +38,52 @@ input.addEventListener("keyup", function(event) {
   });
 
 function newElement() {
-  var li = document.createElement("li");
+  var days_deadline="";
   var inputValue = document.getElementById("myInput").value;
-  var t = document.createTextNode(inputValue);
-  li.appendChild(t);
+  var li = document.createElement("li");
+  var h = document.createElement("H3")                // Create a <h3> element
+  var t = document.createTextNode(inputValue);     // Create a text node
+  
+  h.appendChild(t); //add text node inside header
+  li.appendChild(h); //add header inside the li item
+
+
+
   if (inputValue === '') {
     alert("You must write something!");
   } else {
+        var checkBox=document.getElementById("checkbox");
+        if (document.getElementById('date-input').value=='' && checkBox.checked==true)
+        {
+          alert('Enter the date');
+        }
+        else if(checkBox.checked == true){
+          deadline=document.getElementById('date-input').value;
+          console.log(deadline);
+          deadlineDate=new Date(deadline);
+          var now = new Date().getTime();
+          var distance=deadlineDate-now;
+          var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+
+          if(days<0){
+            days_deadline="Deadline Crossed ("+days.toString()+" days )"
+          }
+          else{
+            days_deadline=days.toString()+" days left"
+          }
+          var hd = document.createElement("H4")               
+          var td = document.createTextNode(days_deadline);    
+          hd.appendChild(td); //add text node inside header (containing deadline)
+          li.appendChild(hd); //add deadline inside the li item
+          console.log(days_deadline)
+        }
+
         ullist= document.getElementById("myUL")
         ullist.insertBefore(li,ullist.childNodes[0]);
    
   }
   document.getElementById("myInput").value = "";
+  days_deadline=""
 
   var span = document.createElement("SPAN");
   var txt = document.createTextNode("\u00D7");
@@ -62,5 +96,15 @@ function newElement() {
       var div = this.parentElement;
       div.parentNode.removeChild(div);
     }
+  }
+}
+function addDeadline(){
+  var checkBox=document.getElementById("checkbox");
+  var deadline=document.getElementById("date-input");
+
+  if (checkBox.checked == true){
+    deadline.style.display = "block";
+  } else {
+    deadline.style.display = "none";
   }
 }
